df979717ac3c0fb8e0f42f5722e9ee091ae72e5b
LEv145 <you@example.com>
2022-11-03 23:15:47 +0200

    v 0.5
    Update architecture
    Overfitting to tutorial04

--- src/layout/urls.py
+++ src/layout/urls.py
@@ -2,12 +2,22 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import RegisterView, NewEventsView, EventView, MainView
+from .views import (
+    RegisterView,
+    NewEventsView,
+    EventView,
+    MainView,
+    event_view,
+    events_view,
+)
 
 
+app_name = "layout"
 urlpatterns = [
     path("", MainView.as_view()),
-    path("registration", RegisterView.as_view()),
-    path("events", NewEventsView.as_view()),
-    path("event/<int:pk>", EventView.as_view(), name="event")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("events", events_view, name="events"),
+    path("event/<int:pk>", event_view, name="event"),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/layout/views.py
+++ src/layout/views.py
@@ -1,24 +1,42 @@
+import typing as t
+
 from django.views import generic
+from django.http import HttpRequest, HttpResponse, Http404
+from django.shortcuts import render
 
 from .forms import RegisterForm
 from .models import Event
 
 
 class MainView(generic.TemplateView):
-    template_name = "main.html"
+    template_name = "layout/main.html"
 
 
 class RegisterView(generic.FormView):
-    template_name = "register.html"
+    template_name = "layout/register.html"
     form_class = RegisterForm
-    # success_url = '/thanks/'
 
 
 class NewEventsView(generic.ListView):
-    template_name = "new-events.html"
+    template_name = "layout/events.html"
     model = Event
 
 
 class EventView(generic.DetailView):
-    template_name = "event.html"
+    template_name = "layout/event.html"
     model = Event
+
+
+def events_view(request: HttpRequest) -> HttpResponse:
+    object_list = Event.objects.order_by("-start_at")[:5]
+
+    return render(request, "layout/events.html", {"object_list": object_list})
+
+
+def event_view(request: HttpRequest, pk: int) -> HttpResponse:
+    try:
+        object_ = Event.objects.get(pk=pk)
+    except Event.DoesNotExist:
+        raise Http404()
+
+    return render(request, "layout/event.html", {"object": object_})
